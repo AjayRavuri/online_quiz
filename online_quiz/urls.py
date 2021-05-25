c0e66dc645a85a7861ae286a34caf5e86bb173df
Ajay Ravuri <ajayravuri3@gmail.com>
2021-05-25 12:43:46 +0530

    login added

--- online_quiz/urls.py
+++ online_quiz/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from quiz import views
+from quiz.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', login,name="login"),
 ]
