fcaa0c307feb9a858354a1cf9c56d39957f4cbc6
Saiteja-Perina <perina.poornasaiteja@gmail.com>
2021-05-26 11:32:27 +0530

    Integrated

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
-    return render(request,'login.html')
+	if request.method=='POST':
+		user=request.POST['email']
+		pwd=request.POST['password']
+		try:
+			data=User.objects.get(mail=user,password=pwd)
+			l="<html><head><title>Success</title></head><body><h1 style='color:#fa5432; text-align:center'>Success</h1></body></html>"
+			return HttpResponse(l)
+		except:
+			messages.info(request,'Invalid credentials')
+			return redirect(login)
+	return render(request,'login.html')
